de3c9be8abfd65af4567f6864138482c75f66d86
MoonFlower18 <90007550+MoonFlower18@users.noreply.github.com>
2024-01-07 00:24:29 +0400

    проба

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const { google } = require("googleapis");
 
 const app = express();
+app.use(cors());
+//=====================================
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
